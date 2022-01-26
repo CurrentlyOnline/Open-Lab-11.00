647908f7985e033eb99c654d65d5ff24bbd1667f
CurrentlyOnline <sambor433@gmail.com>
2022-01-26 22:15:26 +0100

    Add project files.

---
+++ Open-Lab-11.00/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_11._00
+{
+    class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public static List<string> categoryList = new List<string>
+        {
+            "detské", "romantické", "náučné", "sci-fi", "dobrodružné"
+        };
+
+        public Book()
+        {
+            Title = "-1";
+            Pages = -1;
+            Category = "-1";
+            Author = "-1";
+            ReleaseDate = -1;
+        }
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+            Category = "-1";
+            Author = "-1";
+            ReleaseDate = -1;
+        }
+        public Book(string title, int pages, string category, string author, int releaseDate)
+        {
+            Title = title;
+            Pages = pages;
+            Category = category;
+            Author = author;
+            ReleaseDate = releaseDate;
+        }
+        public string Title { get; set; }     
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    pages = value;
+                }
+                else
+                {
+                    pages = 1;
+                }
+            }
+        }
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+        public int ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                if (value <= 2021 && value >= 1450)
+                {
+                    releaseDate = value;
+                }
+                else
+                {
+                    releaseDate = -1;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return Title + "\n" + Pages + "\n" + Category + "\n" + Author + "\n" + releaseDate;
+        }
+    }
+}
---
+++ Open-Lab-11.00/Library.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_11._00
+{
+    class Library
+    {
+        int numberOfBooks;
+        int numberOfStudents;
+        List<string> studentsList = new List<string>();
+        List<Book> booksList = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            booksList.Add(book);
+        }
+        public void AddStudent(string NewStudent)
+        {
+            studentsList.Add(NewStudent);
+        }
+        public void Write()
+        {
+            numberOfBooks = booksList.Count;
+            numberOfStudents = studentsList.Count;
+
+            Console.WriteLine(numberOfStudents + numberOfBooks);
+        }
+
+    }
+}
